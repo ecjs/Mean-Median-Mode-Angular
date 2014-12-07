c2feb0267beb1484473f8e3b9d55b52d7d31ba3c
Mark Harrell <mark@mjharrell.com>
2014-12-06 17:35:11 -0800

    services add

--- app/js/client.js
+++ app/js/client.js
@@ -1,5 +1,7 @@
 require('angular/angular');
 
 var mmmApp = angular.module('mmmApp', []);
-
+//controllers
 require('./controllers/mmm_controller')(mmmApp);
+//services
+require('./services/mmm_service')(mmmApp);

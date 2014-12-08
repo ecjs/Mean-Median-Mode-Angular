b9bead2befe64f6e5bab329f567efef80316a551
Mark Harrell <mark@mjharrell.com>
2014-12-08 10:28:29 -0800

    move server calls to local calls

--- app/js/services/mmm_service.js
+++ app/js/services/mmm_service.js
@@ -1,24 +1,57 @@
 module.exports = function(app) {
 
-  app.factory('mmmService', ['$http', function($http) {
+  app.factory('mmmService', function() {
     return {
       returnMMM: function(numsArray, scope) {
-        console.log('numsArray before its sent to api: ' + numsArray);
-        $http({
-          method: 'POST',
-          url: '/mmm',
-          data: numsArray
-        })
-        .success(function(data) {
-          scope.mmm.mean = data.mean;
-          scope.mmm.median = data.median;
-          scope.mmm.mode = data.mode;
-          scope.hiddenMMM = false;
-        })
-        .error(function(data) {
-          console.log(data);
-        });
+        var meanFind = function(array) {
+          var sum = 0;
+          for (var i = 0; i < array.length; i++) {
+            sum += array[i];
+          }
+          return sum / array.length;
+        };
+
+        var modeFind = function(array) {
+          var count = {};
+          var highest = 0;
+          var winner = [];
+          for (var i = 0; i < array.length; i++) {
+            var num = array[i];
+            count[num] = (count[num] || 0) + 1;
+          }
+
+          for (var prop in count) {
+            if (count[prop] >= highest) {
+              highest = count[prop];
+            }
+          }
+
+          for (var property in count) {
+            if (count[property] == highest) {
+              winner.push(property);
+            }
+          }
+          return winner.toString();
+        };
+
+        var medianFind = function(array) {
+          var halved = array.length / 2;
+          if (array.length % 2 === 0) {
+            return (array[halved] + array[halved - 1]) / 2;
+          }
+          else {
+            return array[Math.round(halved) - 1];
+          }
+        };
+          // scope.mmm.mean = data.mean;
+          // scope.mmm.median = data.median;
+          // scope.mmm.mode = data.mode;
+          // scope.hiddenMMM = false;
+        scope.mmm.mean = meanFind(numsArray);
+        scope.mmm.median = medianFind(numsArray);
+        scope.mmm.mode = modeFind(numsArray);
+        scope.hiddenMMM = false;
       }
     };
-  }]);
+  });
 };
--- test/client/mmm_tests.js
+++ test/client/mmm_tests.js
@@ -19,29 +19,19 @@ describe('mmmController', function() {
   });
 
   describe('rest request', function() {
-    beforeEach(angular.mock.inject(function(_$httpBackend_) {
-      $httpBackend = _$httpBackend_;
-    }));
-
-    afterEach(function() {
-      $httpBackend.verifyNoOutstandingExpectation();
-      $httpBackend.verifyNoOutstandingRequest();
-    });
 
     it('make a call to mmm', function() {
-      $httpBackend.expectPOST('/mmm').respond(200, {mean: 6, median: 6.5, mode: '5'});
 
       $controllerConstructor('mmmCtrl', {$scope: $scope});
       $scope.mmm = {};
       $scope.mmm.numbers = '5 5 6 8';
       $scope.sendMMM();
-      $httpBackend.flush();
 
       expect($scope.mmm.mean).toBeDefined();
       expect($scope.mmm.median).toBeDefined();
       expect($scope.mmm.mode).toBeDefined();
       expect($scope.mmm.mean).toBe(6);
-      expect($scope.mmm.median).toBe(6.5);
+      expect($scope.mmm.median).toBe(5.5);
       expect($scope.mmm.mode).toBe('5');
       expect($scope.hiddenMMM).toBe(false);
     });
